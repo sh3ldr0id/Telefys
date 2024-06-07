181c80f0af64b9c53322e2a3ef9dfe62138f5135
Sheldon Sinu <sh3ldr0id@gmail.com>
2024-06-07 19:21:05 +0300

    Auto restart on error

--- run.py
+++ run.py
@@ -1,6 +1,15 @@
 from app import bot
 
-bot.polling(
-    timeout=100,
-    long_polling_timeout=100
-)
+if __name__ == "__main__":
+    while True:
+        try:
+            print("Server Running !")
+
+            bot.polling()
+
+            print("Server Stopped !")
+            
+            break
+
+        except Exception as e:
+            print(e)
